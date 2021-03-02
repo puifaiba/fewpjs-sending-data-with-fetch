4f078e7315fb14a2506ff9d3ff23ed5d3f67c351
puifaiba <puifai.ba@gmail.com>
2021-03-02 01:26:42 -0500

    commit tree 7b1d6f38c7e320473b6841cf6da5570915144346

--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-// Add your code here
+// let configObj = {
+//   method: "POST",
+//   header: {
+//     "Content-Type": "application/json",
+//     "Accept": "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// let formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// fetch("http://localhost:3000/dogs", configObj)
+//     .then(function(response) {
+//         return response.json();
+//     })
+//     .then(function(object) {
+//         console.log(object);
+//     })
+//     .catch(function(error) {
+//         alert("Baddd tings")
+//         console.log(error.message)
+//     });
+
+function submitData(personName, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    header: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then((response) => response.json())
+    .then((personObj) => document.body.append(personObj.id))
+    .catch(function (error) {
+      alert("Oh no");
+      document.body.append(error.message);
+    });
+}
+
+// let configObj(personName, email) {
+//   return {
+//     method: "POST",
+//     header: {
+//       "Content-Type": "application/json",
+//       Accept: "application/json",
+//     },
+//     body: JSON.stringify({
+//       name: personName,
+//       email: email,
+//     }),
+//   };
+// }
